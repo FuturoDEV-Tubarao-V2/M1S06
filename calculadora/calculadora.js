ba3c590698aa7f5e2eb6cea1bfb63d1d5a499829
Eduardo Silva <eduardo.r.da.silva@accenture.com>
2024-03-07 19:53:20 -0300

    Exercicio calculadora parte 4.1 e 4.2

---
+++ calculadora/calculadora.js
@@ -0,0 +1,5 @@
+let numeroA = prompt('Digite o primeiro numero');
+let numeroB = prompt('Digite o segundo numero');
+let operacao = prompt('Digite a operação a ser realizada');
+
+window.alert('A soma dos números é: ' + eval(numeroA + operacao + numeroB));
