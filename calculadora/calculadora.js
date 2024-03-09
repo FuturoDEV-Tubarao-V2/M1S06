8bdbc7c4a5e9637a951fd41265b90c0a6bb6c7a7
Eduardo Silva <eduardo.r.da.silva@accenture.com>
2024-03-08 22:13:34 -0300

    Exercicio calculadora parte 7

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,10 +1,13 @@
 var display = '';
+var operacoes = [];
 
 function agregarDisplay(elemento) {
   this.atualizarDisplay(display += elemento);
 }
 
 function calcularOperacao() {
+  //salva a operacao realizada no historico de 'operacoes'
+  operacoes.push(display + '=' + eval(display));
   this.atualizarDisplay(eval(display));
 }
 
@@ -19,4 +22,35 @@ function atualizarDisplay(valor) {
   //displayInfo.value = '';
   //span
   displayInfo.innerHTML = valor;
+}
+
+function mostrarHistorico() {
+  var listaHistorico = document.getElementById('lista-historico');
+
+  //caso o elemento listaHistorico possua algum elemento filho, ele será removido
+  //isso é feito para que não fique 'sujeira' de elementos exibidos anteriormente (duplicados)
+  while(listaHistorico.firstChild){
+    listaHistorico.removeChild(listaHistorico.firstChild);
+  }
+
+  //iteração na lista de 'operacoes' onde, pra cada 'operacao'
+  //será criado um elemento li, seu conteúdo será atualizado com a operacao realizada
+  //e esse elemento será adicionado no elemento 'listaHistorico'
+  operacoes.forEach(operacao => {
+    var li = document.createElement('li');
+    li.innerHTML = operacao;
+    listaHistorico.appendChild(li);
+  });
+}
+
+function limparHistorico() {
+  var listaHistorico = document.getElementById('lista-historico');
+
+  //limpa as 'operacoes' realizadas
+  operacoes = [];
+
+  //caso o elemento 'listaHistorico' possua algum elemento filho, ele será removido
+  while(listaHistorico.firstChild){
+    listaHistorico.removeChild(listaHistorico.firstChild);
+  }
 }
